b4355f90f92f7de639e9e6a161d2b17aad90543e
DeeBee4 <109605453+DeeBee4@users.noreply.github.com>
2022-07-19 17:48:04 +0300

    Update streamlit_app.py
    formatting the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Healthy Menu')
-streamlit.text('Eggs Benedict - 3 EUR')
-streamlit.text('Guacamole - 2 EUR')
-streamlit.text('Avocado Toast - 2 EUR')
+streamlit.text('Eggs Benedict    - 3 EUR')
+streamlit.text('Guacamole        - 2 EUR')
+streamlit.text('Avocado Toast    - 2 EUR')
 streamlit.text('Cold Brew Coffee - 2 EUR')
-streamlit.text('Espresso - 1 EUR')
+streamlit.text('Espresso         - 1 EUR')
