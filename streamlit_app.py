db5d04d5a3854507dd1343c6c55f18ea3e3bd0a4
DeeBee4 <109605453+DeeBee4@users.noreply.github.com>
2022-07-19 17:40:17 +0300

    Update streamlit_app.py
    Adding menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Healthy Menu')
+streamlit.text('Eggs Benedict - 3 EUR')
+streamlit.text('Guacamole - 2 EUR')
+streamlit.text('Cold Brew Coffee - 2 EUR')
