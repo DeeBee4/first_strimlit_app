29c5fa4133de21597bac7735129f869bc630f9b0
DeeBee4 <109605453+DeeBee4@users.noreply.github.com>
2022-07-20 13:39:57 +0300

    Update streamlit_app.py
    adding a panda dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('| 🥑 Guacamole             - 2 EUR')
 streamlit.text('| 🥑 Avocado Toast         - 2 EUR')
 streamlit.text('| ☕ Cold Brew Coffee      - 2 EUR')
 streamlit.text('| ☕ Espresso              - 1 EUR')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
